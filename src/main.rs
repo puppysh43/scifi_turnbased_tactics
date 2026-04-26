b10eb2256431d7dcd8ab3375a0462bf8f553d37b
quinn <quinn@localhost.localdomain>
2026-04-25 22:56:53 -0400

    switching to egor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use notan::{draw::*, prelude::*};
+
+mod state;
+#[notan_main]
+fn main() -> Result<(), String> {
+    let win_config = WindowConfig::new()
+        .set_size(1080, 1920)
+        .set_fullscreen(true);
+    notan::init_with(setup)
+        .add_config(win_config)
+        .add_config(DrawConfig)
+        .update(update)
+        .draw(draw)
+        .build()
+}
+
+fn update(app: &mut App, state: &mut State) {
+    //
 }
---
+++ src/state.rs
@@ -0,0 +1,8 @@
+/*
+gamestate/appstate. originally will just be for a basic pre-programmed game but ideally I want the player to be able
+to make their own scenarios with a map editor, and a scenario selection screen.
+*/
+#[derive(AppState)]
+pub struct State {
+    world: World,
+}
